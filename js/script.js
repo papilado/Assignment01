250fdfcdf965984dd075f5a1c686d027340045aa
Patrick Apilado <papilado@gmail.com>
2020-02-06 20:22:59 -0800

    assignment1

---
+++ js/script.js
@@ -0,0 +1,164 @@
+/* STEP 1
+var someMonth;
+function theMonth();
+var currentMonth;
+var summerMonth;
+var myLibraryFunction;
+*/
+
+/* STEP 2
+let total = 4;
+let name = 'Patrick';
+let myName = true;
+let total = null;
+*/
+
+/* STEP 3
+let anExpression = 3 * (4/5) + 6;
+let aSecondExpression = Math.PI * radius * radius;
+*/
+
+/* STEP 4
+let firstName;
+let lastName;
+let address;
+let city;
+let state;
+let zipCode;
+let age;
+let referralSource;
+let mayWeContactYou;
+*/
+
+/* STEP 5
+let firstName;
+firstName = 'Patrick';
+let firstName = 'Patrick';
+
+let lastName;
+lastName = 'Apilado';
+let lastName = 'Apilado';
+
+let address;
+address = '6455 Paradise';
+let address = '6455 Paradise';
+
+let city;
+city = 'San Diego';
+let city = 'San Diego';
+
+let state;
+state = 'California';
+let state = 'California';
+
+let zipCode;
+zipCode = '92114';
+let zipCode = '92114';
+
+let age;
+age = '34';
+let age = '34';
+
+let referralSource;
+referralSource = 'Company';
+let referralSource = 'Company';
+
+let mayWeContactYou;
+mayWeContactYou = true;
+let mayWeContactYou = true;
+*/
+
+/* STEP 6
+let age = 34;
+window.console.log('I am ' + age + ' years old.');
+*/
+
+/* STEP 7
+let value1;
+value1 = true;
+window.console.log('This boolean is ' + value1);
+
+let value2;
+value2 = false;
+window.console.log(10 + value2);
+*/
+
+/* STEP 8
+Not valid. 
+var someString = 'Who once said, '\'' + Only two things are infinite, the universe and human stupidity, and I'm not sure about the former. '\'';
+*/
+
+/* STEP 9
+let firstName = null;
+window.console.log(firstName);
+
+let lastName = undefined;
+window.console.log(lastName);
+*/
+
+/* STEP 10
+window.console.log(typeof 'Patrick');
+window.console.log(typeof 99);
+window.console.log(typeof true);
+window.console.log(typeof Object());
+window.console.log(typeof undeclaredVariable);
+*/
+
+/* STEP 11
+let firstName = 'Patrick';
+let lastName = 'Apilado';
+alert('Hello ' + firstName + ' ' + lastName + ' , welcome to the JavaScript class!');
+*/
+
+/* STEP 12
+let name = "Patrick Apilado";
+alert('Hello ' + name + ' , welcome to the JavaScript class!');
+*/
+
+/* STEP 13
+let course = 'JavaScript';
+let greeting = 'Hello ';
+let name = 'Patrick';
+let secondGreeting = ', welcome to the ';
+let lastWord = ' class!';
+alert(greeting + name + secondGreeting + course + lastWord);
+*/
+
+/* STEP 14
+let course = 'JavaScript';
+let greeting = 'Hello ';
+let name = 'Patrick.';
+let secondGreeting = 'Welcome to the ';
+let lastWord = ' class!';
+alert(greeting + name + '\n' + secondGreeting + course + lastWord);
+*/
+
+// STEP 15
+// STEP 16
+
+/* STEP 17
+let x = 10;
+let y = 20;
+window.console.log(x + y);
+*/
+
+/* STEP 18
+let x = 20;
+window.console.log(x + 20);
+*/
+
+/* STEP 19
+let x = 20;
+window.console.log(x * 5);
+*/
+
+/* STEP 20
+let x = 20 % 3;
+window.console.log(x / 1);
+*/
+
+// STEP 21
+// STEP 22
+// STEP 23
+// STEP 24
+// STEP 25
